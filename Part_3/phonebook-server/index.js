35673808ec430b654946c9bdd5e375bcb14f3620
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-19 19:36:27 +0545

    feat: phoneserver is initialized and setup

---
+++ Part_3/phonebook-server/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express();
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).send('Data not Found!')
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number ) {
+    return response.status(400).json({
+      error: 'No name or number'
+    })
+  } 
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({error: 'name must be unique'})
+  }
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id : String(Math.floor(Math.random() * 1000000))
+  }
+
+  persons = persons.concat(person)
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const info = `Phonebook has info for ${persons.length} people
+  <p>${new Date()}</p>`
+    response.send(info)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
