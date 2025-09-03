5de2c06cdf312dad470423f1798c7e3dfcf78e05
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-09-03 21:00:09 +0545

    feat: changes added

--- Part_4/Blog/app.js
+++ Part_4/Blog/app.js
@@ -8,12 +8,23 @@ const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
+logger.info('connecting to', config.MONGODB_URL)
+
 mongoose.connect(config.MONGODB_URL)
+  .then(() => {
+  logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+  logger.error('error connection to MongoDB:', error.message)
+})
 
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.use(express.json())
 
--- Part_4/Blog/utils/logger.js
+++ Part_4/Blog/utils/logger.js
@@ -6,5 +6,4 @@ const error = (...params) => {
   console.error(...params)
 }
 
-
 module.exports = { info, error }
