ecdd2d163fe50a088fe5b60748cc888d6ea52711
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-09-04 07:42:31 +0545

    feat: Part4.7 completed

--- Part_4/Blog/tests/list_helper.test.js
+++ Part_4/Blog/tests/list_helper.test.js
@@ -1,6 +1,6 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
 
 // test('dummy returns one', () => {
 //   const blogs = []
@@ -9,122 +9,167 @@ const listHelper = require('../utils/list_helper')
 //   assert.strictEqual(result, 1)
 // })
 
-describe('total likes', () => {
+describe("total likes", () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 5,
-      __v: 0
-    }
-  ]
+      __v: 0,
+    },
+  ];
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
-})
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.deepStrictEqual(result, 5);
+  });
+});
 
-describe('favorite blog', () => {
+describe("favorite blog", () => {
   const blogs = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Harmful',
-      author: 'Edsger Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Harmful",
+      author: "Edsger Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 1005,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'Go To Considered Harmful',
-      author: 'Edsger',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Go To Considered Harmful",
+      author: "Edsger",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 95,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a422aa71b54a676234d17f5',
-      title: 'Go Considered Harmful',
-      author: 'W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f5",
+      title: "Go Considered Harmful",
+      author: "W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 2005,
-      __v: 0
+      __v: 0,
     },
-  ]
+  ];
 
-  test('of empty list is null', () => {
-    assert.deepStrictEqual(listHelper.favoriteBlog([]), null)
-  })
-
-  test('of many is the one with most likes', () => {
-    const result = listHelper.favoriteBlog(blogs)
+  test("of many is the one with most likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
     assert.deepStrictEqual(result, {
-      _id: '5a422aa71b54a676234d17f5',
-      title: 'Go Considered Harmful',
-      author: 'W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f5",
+      title: "Go Considered Harmful",
+      author: "W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 2005,
-      __v: 0
-    })
-  })
-})
+      __v: 0,
+    });
+  });
+});
 
-describe('most blogs', () => {
+describe("most blogs", () => {
   const blogs = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Harmful',
-      author: 'Mahesh Chaudhary',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Harmful",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 1005,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a422aa71b54a676234d17f9',
-      title: 'Go To Considered Harmful',
-      author: 'Edsger',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Go To Considered Harmful",
+      author: "Edsger",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 95,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a439aa74b54a676234d37f6',
-      title: 'Muna Madan',
-      author: 'Mahesh Chaudhary',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a439aa74b54a676234d37f6",
+      title: "Muna Madan",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 20000,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a422aa71b54a676234d17f5',
-      title: 'Go Considered Harmful',
-      author: 'W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d17f5",
+      title: "Go Considered Harmful",
+      author: "W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 2005,
-      __v: 0
+      __v: 0,
     },
     {
-      _id: '5a422aa71b54a676234d27f6',
-      title: 'Jungle of Hurdle',
-      author: 'Mahesh Chaudhary',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      _id: "5a422aa71b54a676234d27f6",
+      title: "Jungle of Hurdle",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
       likes: 200,
-      __v: 0
+      __v: 0,
     },
-  ]
+  ];
 
-  test('empty list returns null', () => {
-    assert.deepStrictEqual(listHelper.mostBlogs([]), null)
-  })
-
-  test('author with most blogs', () => {
-    const result = listHelper.mostBlogs(blogs)
+  test("author with most blogs", () => {
+    const result = listHelper.mostBlogs(blogs);
     assert.deepStrictEqual(result, {
       author: "Mahesh Chaudhary",
       blogs: 3,
-    })
-  })
-})
+    });
+  });
+});
+
+describe("most likes", () => {
+  const blogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Harmful",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 1005,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Go To Considered Harmful",
+      author: "Edsger",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 95,
+      __v: 0,
+    },
+    {
+      _id: "5a439aa74b54a676234d37f6",
+      title: "Muna Madan",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 20000,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f5",
+      title: "Go Considered Harmful",
+      author: "W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 2005,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d27f6",
+      title: "Jungle of Hurdle",
+      author: "Mahesh Chaudhary",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 200,
+      __v: 0,
+    },
+  ];
+
+  test("author with most likes", () => {
+    const result = listHelper.mostLikes(blogs);
+    assert.deepStrictEqual(result, {
+      author: "Mahesh Chaudhary",
+      likes: 21205,
+    });
+  });
+});
--- Part_4/Blog/utils/list_helper.js
+++ Part_4/Blog/utils/list_helper.js
@@ -1,44 +1,56 @@
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((acc, blog) => (
-        sum = acc + blog.likes
-    ),0)
-}
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0);
+};
 
 const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
-    return blogs.reduce((acc, blog) => (
-        blog.likes > acc.likes ? blog : acc
-    ))
-}
+  return blogs.reduce((acc, blog) => (blog.likes > acc.likes ? blog : acc));
+};
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) {
-      return null
+  const blogMap = {};
+
+  blogs.forEach((blog) => {
+    blogMap[blog.author] = (blogMap[blog.author] || 0) + 1;
+  });
+
+  let topAuthor = "";
+  let maxBlogs = 0;
+
+  for (const author in blogMap) {
+    if (blogMap[author] > maxBlogs) {
+      topAuthor = author;
+      maxBlogs = blogMap[author];
+    }
   }
+  return { author: topAuthor, blogs: maxBlogs };
+};
 
-  const blogMap = {}
+const mostLikes = (blogs) => {
+  const likeMap = {};
+  blogs.forEach((blog) => {
+    likeMap[blog.author] = (likeMap[blog.author] || 0) + blog.likes;
+  });
 
-  blogs.forEach(blog => {
-    blogMap[blog.author] = (blogMap[blog.author] || 0) + 1
-  })
+  let topAuthor = "";
+  let maxLikes = 0;
 
-  const topAuthor = Object.keys(blogMap).reduce((max, author)=> {
-    return blogMap[author] > blogMap[max] ? author : max
-  })
-
-  return {
-    author: topAuthor,
-    blogs: blogMap[topAuthor]
+  for (const author in likeMap) {
+    if (likeMap[author] > maxLikes) {
+      topAuthor = author;
+      maxLikes = likeMap[author];
+    }
   }
-}
-
+  return { author: topAuthor, likes: maxLikes };
+};
 
 module.exports = {
-  dummy, totalLikes, favoriteBlog, mostBlogs
-}
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
