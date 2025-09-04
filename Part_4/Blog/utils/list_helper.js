8876bd8437a6ec6544b1aba32472fa062f3ffab3
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-09-04 08:41:58 +0545

    feat: implemented lodash in part4a

--- Part_4/Blog/utils/list_helper.js
+++ Part_4/Blog/utils/list_helper.js
@@ -1,50 +1,71 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1;
 };
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((acc, blog) => acc + blog.likes, 0);
+  // return blogs.reduce((acc, blog) => acc + blog.likes, 0);
+
+  // Using Lodash Library
+  return _.sumBy(blogs, 'likes')
 };
 
 const favoriteBlog = (blogs) => {
-  return blogs.reduce((acc, blog) => (blog.likes > acc.likes ? blog : acc));
+  // return blogs.reduce((acc, blog) => (blog.likes > acc.likes ? blog : acc));
+
+  // Using Lodash Library
+  return _.maxBy(blogs,'likes')
 };
 
 const mostBlogs = (blogs) => {
-  const blogMap = {};
+  // const blogMap = {};
 
-  blogs.forEach((blog) => {
-    blogMap[blog.author] = (blogMap[blog.author] || 0) + 1;
-  });
+  // blogs.forEach((blog) => {
+  //   blogMap[blog.author] = (blogMap[blog.author] || 0) + 1;
+  // });
 
-  let topAuthor = "";
-  let maxBlogs = 0;
+  // let topAuthor = "";
+  // let maxBlogs = 0;
 
-  for (const author in blogMap) {
-    if (blogMap[author] > maxBlogs) {
-      topAuthor = author;
-      maxBlogs = blogMap[author];
-    }
-  }
-  return { author: topAuthor, blogs: maxBlogs };
+  // for (const author in blogMap) {
+  //   if (blogMap[author] > maxBlogs) {
+  //     topAuthor = author;
+  //     maxBlogs = blogMap[author];
+  //   }
+  // }
+  // return { author: topAuthor, blogs: maxBlogs };
+
+  // Using Lodash Library
+
+  const countBlog = _.countBy(blogs, 'author')
+  const countAuthor = _.maxBy(Object.keys(countBlog), author => countBlog[author])
+  return { author: countAuthor, blogs: countBlog[countAuthor]}
 };
 
 const mostLikes = (blogs) => {
-  const likeMap = {};
-  blogs.forEach((blog) => {
-    likeMap[blog.author] = (likeMap[blog.author] || 0) + blog.likes;
-  });
+  // const likeMap = {};
+  // blogs.forEach((blog) => {
+  //   likeMap[blog.author] = (likeMap[blog.author] || 0) + blog.likes;
+  // });
 
-  let topAuthor = "";
-  let maxLikes = 0;
+  // let topAuthor = "";
+  // let maxLikes = 0;
 
-  for (const author in likeMap) {
-    if (likeMap[author] > maxLikes) {
-      topAuthor = author;
-      maxLikes = likeMap[author];
-    }
-  }
-  return { author: topAuthor, likes: maxLikes };
+  // for (const author in likeMap) {
+  //   if (likeMap[author] > maxLikes) {
+  //     topAuthor = author;
+  //     maxLikes = likeMap[author];
+  //   }
+  // }
+  // return { author: topAuthor, likes: maxLikes };
+
+  // Using Lodash Library
+
+  const countLikes = _.groupBy(blogs, 'author')
+  const countAuthor = _.maxBy(Object.keys(countLikes), author => _.sumBy(countLikes[author], 'likes'))
+
+  return {author: countAuthor, likes: _.sumBy(countLikes[countAuthor], 'likes')}
 };
 
 module.exports = {
