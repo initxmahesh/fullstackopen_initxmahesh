1c30acc20e82fe0d7aadc74a720560f414508de2
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-09-30 10:35:58 +0545

    feat: Part6.10 is completed

--- Part_6/redux-anecdotes/src/reducers/filterReducer.js
+++ Part_6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,32 @@
-const filterReducer = (state = "", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
+// const filterReducer = (state = "", action) => {
+//   switch (action.type) {
+//     case "SET_FILTER":
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export const setFilter = (filter) => {
+//   return {
+//     type: "SET_FILTER",
+//     payload: filter,
+//   };
+// };
+
+// export default filterReducer;
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    setFilter(state, action) {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    },
+  },
+});
 
-export const setFilter = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
-
-export default filterReducer;
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
