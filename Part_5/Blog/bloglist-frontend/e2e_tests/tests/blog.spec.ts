0a0ed3a56566b0532cf68d017884171670c99cb9
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-10-02 20:02:55 +0545

    feat: added the helper function and the part5.19 test is completed

--- Part_5/Blog/bloglist-frontend/e2e_tests/tests/blog.spec.ts
+++ Part_5/Blog/bloglist-frontend/e2e_tests/tests/blog.spec.ts
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog app", () => {
   let userdb;
@@ -23,18 +24,41 @@ describe("Blog app", () => {
 
   describe("Login", () => {
     test("succeeds with correct credentials", async ({ page }) => {
-      await page.getByLabel("username").fill(userdb.username);
-      await page.getByLabel("password").fill(userdb.password);
-      await page.getByRole("button", { name: /login/i }).click();
+      //   await page.getByLabel("username").fill(userdb.username);
+      //   await page.getByLabel("password").fill(userdb.password);
+      //     await page.getByRole("button", { name: /login/i }).click();
+      await loginWith(page, userdb.username, userdb.password);
+
       await expect(page.getByText("maheshfirst logged in")).toBeVisible();
     });
 
     test("fails with wrong credentials", async ({ page }) => {
-      await page.getByLabel("username").fill(userdb.username);
-      await page.getByLabel("password").fill("wrong");
-      await page.getByRole("button", { name: /login/i }).click();
+      //   await page.getByLabel("username").fill(userdb.username);
+      //   await page.getByLabel("password").fill("wrong");
+      //   await page.getByRole("button", { name: /login/i }).click();
+      await loginWith(page, userdb.username, "wrong");
 
       await expect(page.getByText(/wrong username or password/i)).toBeVisible();
+    });
+  });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, userdb.username, userdb.password);
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "create new blog" }).click();
+
+      await page.getByLabel("title:").fill("Test Blog Title");
+      await page.getByLabel("author:").fill("Test Author");
+      await page.getByLabel("url:").fill("http://testblog.com");
+
+      await page.getByRole("button", { name: /create/i }).click();
+
+      await expect(
+        page.getByText("Test Blog Title by Test Author")
+      ).toBeVisible();
     });
   });
 });
