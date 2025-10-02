4e412f88c9f126ee2e5f6ee1ce058dd6450029bf
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-10-02 16:16:30 +0545

    feat: all test cases for part5-c is done

--- Part_5/Blog/bloglist-frontend/src/components/Blog.test.jsx
+++ Part_5/Blog/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
+import { expect, vi } from "vitest";
+import CreateBlog from "./CreateBlog";
 
 const blog = {
   title: "Component testing is done with react-testing-library",
@@ -38,4 +40,70 @@ test("renders title", () => {
   );
   screen.debug(element);
   expect(element).toBeDefined();
+});
+
+test("renders title and author, but not url or likes by default", () => {
+  render(<Blog blog={blog} />);
+
+  const titleEle = screen.getByText(
+    "Component testing is done with react-testing-library Test Author"
+  );
+  expect(titleEle).toBeInTheDocument();
+
+  const urlEle = screen.queryByText("https://testurl.com");
+  expect(urlEle).toBeNull();
+
+  const likesEle = screen.queryByText("likes 5");
+  expect(likesEle).toBeNull();
+});
+
+test("shows url and likes when view button is clicked", async () => {
+  const user = userEvent.setup();
+  render(<Blog blog={blog} />);
+
+  const viewButton = screen.getByText("view");
+  await user.click(viewButton);
+
+  expect(screen.getByText(blog.url)).toBeInTheDocument();
+  expect(screen.getByText(`likes ${blog.likes}`)).toBeInTheDocument();
+});
+
+test("calls event handler twice when like button is clicked twice", async () => {
+  const user = userEvent.setup();
+  const handleLike = vi.fn();
+  render(<Blog blog={blog} handleLike={handleLike} />);
+
+  const viewButton = screen.getByText("view");
+  await user.click(viewButton);
+
+  const likeButton = screen.getByText("like");
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(handleLike).toHaveBeenCalledTimes(2);
+});
+
+test("form calls event handler with correct details when new blog is created", async () => {
+  const handleCreate = vi.fn();
+  const user = userEvent.setup();
+
+  render(<CreateBlog createBlog={handleCreate} />);
+
+  const TitleField = screen.getByLabelText("title:");
+  const AuthorField = screen.getByLabelText("author:");
+  const UrlField = screen.getByLabelText("url:");
+
+  await user.type(TitleField, "Test Blog Title");
+  await user.type(AuthorField, "Test Author");
+  await user.type(UrlField, "http://testurl.com");
+
+  const createButton = screen.getByRole("button", { name: /create/i });
+  await user.click(createButton);
+
+  expect(handleCreate).toHaveBeenCalledTimes(1);
+  expect(handleCreate).toHaveBeenCalledWith({
+    title: "Test Blog Title",
+    author: "Test Author",
+    url: "http://testurl.com",
+  });
 });
