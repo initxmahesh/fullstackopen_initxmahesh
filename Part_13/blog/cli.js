4aea521f46f44c8b96eb994ad35816835e739404
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-11-05 00:21:16 +0545

    Upto Exercise13.4 is completed

--- Part_13/blog/cli.js
+++ Part_13/blog/cli.js
@@ -1,38 +1,29 @@
-require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const { Sequelize, QueryTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   },
-})
+});
 
 const main = async () => {
   try {
     const blogs = await sequelize.query("SELECT * FROM blogs", {
       type: QueryTypes.SELECT,
-    })
+    });
 
     blogs.forEach((blog) => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    })
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
+    });
   } catch (error) {
-    console.error('Unable to connect to the database:', error)
+    console.error("Unable to connect to the database:", error);
   }
-}
+};
 
-main()
-
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-  res.json(blogs)
-})
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+main();
