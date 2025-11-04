6901d3d8390dccb813c637cdc62dd61497749caf
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-11-04 13:39:48 +0545

    Minor changes mades to the code

--- Part_13/blog/cli.js
+++ Part_13/blog/cli.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-
 const { Sequelize, QueryTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -9,15 +8,17 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
-});
+})
 
 const main = async () => {
   try {
-    await sequelize.authenticate()
+    const blogs = await sequelize.query("SELECT * FROM blogs", {
+      type: QueryTypes.SELECT,
+    })
 
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
+    blogs.forEach((blog) => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    })
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
