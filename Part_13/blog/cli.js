63293ed565d072862821e3f7f49784b8fc07bd17
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-11-04 13:43:21 +0545

    get api is done and working

--- Part_13/blog/cli.js
+++ Part_13/blog/cli.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -25,3 +27,12 @@ const main = async () => {
 }
 
 main()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  res.json(blogs)
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
